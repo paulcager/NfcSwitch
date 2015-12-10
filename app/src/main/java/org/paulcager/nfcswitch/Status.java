f4627aa489cc4ea55f4de614cb80bd00e0cdddc0
Paul Cager <paul@paulcager.org>
2015-12-10 20:59:20 +0000

    Initial debug prints

--- app/src/main/java/org/paulcager/nfcswitch/Status.java
+++ app/src/main/java/org/paulcager/nfcswitch/Status.java
@@ -1,7 +1,14 @@
 package org.paulcager.nfcswitch;
 
+import android.bluetooth.BluetoothAdapter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.UnsupportedEncodingException;
 
 public class Status extends AppCompatActivity {
 
@@ -9,5 +16,44 @@ public class Status extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+            Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMsgs != null) {
+                for (int i = 0; i < rawMsgs.length; i++) {
+                    NdefMessage msg = (NdefMessage) rawMsgs[i];
+                    for (int j = 0; j < msg.getRecords().length; j++) {
+                        NdefRecord rec = msg.getRecords()[j];
+                        if ("text/plain".equals(rec.toMimeType())) {
+                            byte[] payload = rec.getPayload();
+                            String textEncoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
+                            int languageCodeLength = payload[0] & 0077;
+                            try {
+                                String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+                                String text =
+                                        new String(payload, languageCodeLength + 1,
+                                                payload.length - languageCodeLength - 1, textEncoding);
+
+                                System.out.println("Text = " + text);
+                            } catch (UnsupportedEncodingException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.disable();
+        } else {
+            bluetoothAdapter.enable();
+        }
     }
 }
